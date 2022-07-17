f7b44ed758891ec782afea193e98aa47adbb0935
d30822tt <d30822t@gmail.com>
2022-07-17 19:55:46 +0900

    サイトのサンプルを参考に日記ページを追加

---
+++ wwwroot/js/ixdb.js
@@ -0,0 +1,22 @@
+﻿// データベースの作成
+var db = new Dexie("diarydb");
+
+// オブジェクトストア（テーブル）の作成
+db.version(1).stores({
+    diary: 'selectDay'
+});
+
+// 指定日の日記を保存する
+function putDiary(val) {
+    db.diary.put(val);
+}
+
+// 指定日の日記を取得する
+function getDiary(key) {
+    return db.diary.get(key);
+}
+
+// 指定範囲のキーを返す
+function getKeys(s, e) {
+    return db.diary.where("selectDay").between(s, e).keys();
+}
